93bec8a2b8dbff2a8dc5cabe20f07cf25b23b5c1
Yomaira Escano <yjescano@gmail.com>
2018-01-23 04:45:03 +0000

    automatically backed up by learn

--- models/item.rb
+++ models/item.rb
@@ -1,16 +1,16 @@
 # class Item
 #   attr_accessor :name, :price
-# 
+#
 #   @@items = []
-# 
+#
 #   def initialize(params)
 #     @name = params[:name]
 #     @price = params[:price]
 #     @@items << self
 #   end
-# 
+#
 #   def self.all
 #     @@items
 #   end
-# 
+#
 # end
