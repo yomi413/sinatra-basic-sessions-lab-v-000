fb6fad52710049b8ccd4b9e70f5ddc1695ce4db6
Yomaira Escano <yjescano@gmail.com>
2018-01-23 04:39:21 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ class App < Sinatra::Base
   post '/checkout' do
     # binding.pry
     @item = Item.new(params[:item])
-    session["item"] = "socks"
+    session["item"] = "#{@item.name}"
     @session = session
 
     erb :checkout
